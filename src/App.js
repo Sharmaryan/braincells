45c392b45b59fb973cc7a184b65535d4db640f09
Aryan Sharma <72251324+Sharmaryan@users.noreply.github.com>
2022-10-21 22:27:00 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import data from "./CardFlip.json";
 
 function App() {
+  const images = data["Card-Flip"][0].imageSet;
+  console.log(images);
+
+
+  // console.log(images[0],'from')
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {/* <img src={ab} /> */}
+      {[
+        "./assets/farm_animals/cow.png",
+        "./assets/farm_animals/rabbit.png",
+        "./assets/farm_animals/dog.png",
+        "./assets/farm_animals/cat.png",
+        "./assets/farm_animals/camel.png",
+        "./assets/farm_animals/sheep.png",
+      ].map((item) => {
+        console.log(item);
+        return <img src={item} alt="helo" />;
+      })}
+      {/* <img src={ab} alt="" /> */}
+    </>
   );
 }
 
