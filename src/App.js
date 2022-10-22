1b7923583309f05fba8cc18c294d4e2ae1e86fc4
Aryan Sharma <72251324+Sharmaryan@users.noreply.github.com>
2022-10-22 09:29:43 +0530

    created JSON

--- src/App.js
+++ src/App.js
@@ -3,25 +3,12 @@ import data from "./CardFlip.json";
 
 function App() {
   const images = data["Card-Flip"][0].imageSet;
-  console.log(images);
 
-
-  // console.log(images[0],'from')
   return (
     <>
-      {/* <img src={ab} /> */}
-      {[
-        "./assets/farm_animals/cow.png",
-        "./assets/farm_animals/rabbit.png",
-        "./assets/farm_animals/dog.png",
-        "./assets/farm_animals/cat.png",
-        "./assets/farm_animals/camel.png",
-        "./assets/farm_animals/sheep.png",
-      ].map((item) => {
-        console.log(item);
-        return <img src={item} alt="helo" />;
+      {images.map((item) => {
+        return <img key={item.id} src={item.image} alt="helo" />;
       })}
-      {/* <img src={ab} alt="" /> */}
     </>
   );
 }
